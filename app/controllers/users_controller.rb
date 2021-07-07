19ca18c6eedc736106197beda59017abd9669893
WadaAtsushi <tsurupika0901@.gmail.com>
2021-07-07 21:04:56 +0900

    新規投稿

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,24 @@ class UsersController < ApplicationController
   end
 
   def signin
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "新規登録しました"
+      redirect_to posts_index_path
+    else
+      flash[:notice] = "入力内容が間違えています"
+      render("users/signin")
+      
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'users/index'
   get 'users/login_form'
-  get 'users/signin'
+  get       'users/signin', to: 'users#signin',    as: 'user_signin'
+  post      'users/create', to: 'users#create',    as: 'user_create'
   get 'posts/edit'
-  get 'posts/index',        to: 'posts#index',     as: 'posts_index'
-  get 'posts/new',          to: 'posts#new',       as: 'post_new'
-  post 'psots/create',      to: 'posts#create',    as: 'post_create'
+  get        'posts/index', to: 'posts#index',     as: 'posts_index'
+  get          'posts/new', to: 'posts#new',       as: 'post_new'
+  post      'psots/create', to: 'posts#create',    as: 'post_create'
   get 'posts/show'
   get '/',                  to: 'posts#top',       as: 'top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
